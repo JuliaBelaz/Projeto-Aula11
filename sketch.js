7010ddc8974a5502a882d6a5b667cfd63373623e
Julia Belaz <110422399+JuliaBelaz@users.noreply.github.com>
2022-08-04 19:07:58 -0300

    Projeto mudado
    O arquivo que mudou foi o sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,27 @@
-
+var sea,ship;
+var sealmg,shiplmg;
 function preload(){
 seaImg = loadImage("sea.png");
-shipImg1 = loadAnimation("ship-1.png","ship-2.png");
-}
-spitName.addImage(seaImg);
-if(sea.x < 0){
-  sea.x = sea.width/2;
+shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
 }
 
 function setup(){
 createCanvas(400,400);
-  
+background("blue");  
+sea=creatSprite(400,200);
+sea.addImage(seaImg);
+sea.scale=0.3;
+ship = createSprite(130,200,30,30);
+ship.addAnimation("movingShip",shipImg1);
+ship.scale =0.25;
+
 }
 
 function draw() {
-background("blue");
 drawSprites();
+sea.velocityX = -5;
+if(sea.x < 0){
+  sea.x = sea.width/2;
+} 
 
- 
 }
